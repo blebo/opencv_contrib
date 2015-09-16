3253dc6c97730542ede9305e6d6897147eb661e7
Adam Gibson <adam.gibson@tc4k.net>
2015-09-16 11:37:45 +0800

    Initial fixes to Python samples for Python 2/3 compatibility.

--- samples/python2/seeds.py
+++ samples/python2/seeds.py
@@ -9,6 +9,9 @@ Usage:
 
 '''
 
+# Python 2/3 compatibility
+from __future__ import print_function
+
 import numpy as np
 import cv2
 
@@ -20,7 +23,7 @@ import sys
 
 
 if __name__ == '__main__':
-    print __doc__
+    print(__doc__)
 
     try:
         fn = sys.argv[1]
